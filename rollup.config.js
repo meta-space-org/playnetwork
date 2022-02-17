c230acbf4bcc69d9750c8ed9e3c58129ca2ce32b
Aivars Stolers <sarletor@gmail.com>
2022-02-17 12:30:57 +0200

    Added auto-rollup for debug

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import { babel } from '@rollup/plugin-babel';
 import replace from '@rollup/plugin-replace';
 
-const config = {
+const config = [{
     input: 'src/client/playcanvas-network.js',
     output: {
         dir: 'dist',
@@ -9,12 +9,28 @@ const config = {
     },
     treeshake: false,
     plugins: [
-        babel({ babelHelpers: 'bundled' }),
+        babel({
+            babelHelpers: 'bundled',
+            babelrc: false,
+            comments: false,
+            compact: false,
+            minified: false
+        }),
         replace({
             preventAssignment: true,
             $1: ''
         })
     ]
-};
+}, {
+    input: 'src/client/network-entity.js',
+    output: {
+        dir: 'dist',
+        format: 'esm'
+    },
+    treeshake: false,
+    plugins: [
+        babel({ babelHelpers: 'bundled' })
+    ]
+}];
 
 export default config;
